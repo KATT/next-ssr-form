86fa2479d56f8acdc4814e9ff2af172ac7f0c32e
KATT <alexander@n1s.se>
2020-12-26 18:05:19 +0100

    typescript

---
+++ pages/app.tsx
@@ -0,0 +1,34 @@
+import type { AppProps } from "next/app";
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
+import React, { useEffect } from "react";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string, opts: any) => {
+      console.log(`App is changing to ${url} with opts`, opts);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <title>JS is PHP</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
