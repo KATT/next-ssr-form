d23f8a0aa9c98a6aaffb53bdb5b080ef920dcb33
KATT <alexander@n1s.se>
2020-12-26 20:21:50 +0100

    layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const routes: [string, string][] = [
+  ["/", "Vanilla"],
+  ["/formik", "Formik"],
+];
+
+export function Layout(props: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className='wrapper'>
+        <nav>
+          {routes.map(([to, title]) => (
+            <Link href={to} key={to}>
+              <a key={to} className={to === router.asPath ? "active" : ""}>
+                {title}
+              </a>
+            </Link>
+          ))}
+        </nav>
+        <main>{props.children}</main>
+        <footer>
+          <p>
+            Made by <a href='https://twitter.com/alexdotjs'>@alexdotjs</a>.
+            Source at{" "}
+            <a href='https://github.com/KATT/js-is-php'>
+              github.com/KATT/js-is-php
+            </a>
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+}
